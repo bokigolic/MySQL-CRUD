b3f7775e3a32dc3171f9ce05f9c30ccd41405661
bokigolic <bokigolic32@gmail.com>
2022-10-31 12:26:22 -0400

    update and delete works

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,51 +1,70 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from "axios"
-
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Update = () => {
-
-  const preset = {
+  const [book, setBook] = useState({
     title: "",
     desc: "",
     price: null,
-    cover: ""
-  }
+    cover: "",
+  });
+  const [error,setError] = useState(false)
 
-  const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const [book, setBook] = useState(preset);
-
+  const bookId = location.pathname.split("/")[2];
 
   const handleChange = (e) => {
-    setBook(prev => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const handleClick = async e => {
-    e.preventDefault()
+  const handleClick = async (e) => {
+    e.preventDefault();
 
     try {
-      await axios.post("http://localhost:8800/books", book)
-      navigate("/")
+      await axios.put(`http://localhost:8800/books/${bookId}`, book);
+      navigate("/");
     } catch (err) {
-      console.log(err)
+      console.log(err);
+      setError(true);
     }
+  };
 
-  }
-
-  console.log(book)
   return (
-    <div className='form'>
-      <h1>Update book list</h1>
-      <input type="text" placeholder="title" onChange={handleChange} name="title" />
-      <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
-      <input type="number" placeholder="price" onChange={handleChange} name="price" />
-      <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
-
-      <button className='formButton' onClick={handleClick}>Update</button>
+    <div className="form">
+      <h1>Update the Book</h1>
+      <input
+        type="text"
+        placeholder="Book title"
+        name="title"
+        onChange={handleChange}
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Book desc"
+        name="desc"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Book price"
+        name="price"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Update</button>
+      {error && "Something went wrong!"}
+      <Link to="/">See all books</Link>
     </div>
-
-  )
-}
+  );
+};
 
 export default Update;
