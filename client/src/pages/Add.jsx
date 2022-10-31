511c757da95e345b960e2aac9ae3bccb784dcea9
bokigolic <bokigolic32@gmail.com>
2022-10-31 10:06:09 -0400

    Add new book wokrs

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from "axios"
+
 
 const Add = () => {
+
+  const preset = {
+    title: "",
+    desc: "",
+    price: null,
+    cover: ""
+  }
+
+  const navigate = useNavigate()
+
+  const [book, setBook] = useState(preset);
+
+
+  const handleChange = (e) => {
+    setBook(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const handleClick = async e => {
+    e.preventDefault()
+
+    try {
+      await axios.post("http://localhost:8800/books", book)
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+  console.log(book)
   return (
-    <div>Add</div>
+    <div className='from'>
+      <h1>Add new book</h1>
+      <input type="text" placeholder="title" onChange={handleChange} name="title" />
+      <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
+      <input type="number" placeholder="price" onChange={handleChange} name="price" />
+      <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+
+      <button onClick={handleClick}>Add</button>
+    </div>
+
   )
 }
 
