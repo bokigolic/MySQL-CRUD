754e3de4976527c941075e35b5dcb99011b73771
bokigolic <bokigolic32@gmail.com>
2022-10-31 11:24:59 -0400

    delete method works

--- backend/index.js
+++ backend/index.js
@@ -34,23 +34,31 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)"
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
 
   const values = [
     req.body.title,
-    req.body.description,
+    req.body.desc,
     req.body.price,
-    req.body.cover
-
-  ];
+    req.body.cover,
+  ]
 
   db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been created ");
+    if (err) return res.send(err);
+    return res.json(data);
   });
 });
 
 
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend ")
